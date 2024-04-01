bf5690a062ff4bea9027406691ad6d6463890d64
Karan <104277479+karankris@users.noreply.github.com>
2024-04-01 19:43:35 +0530

    Update index.js

--- A) Hoisting/index.js
+++ A) Hoisting/index.js
@@ -1,31 +1,53 @@
-// ---- hoisting---
-// getName() 
-// getName2()
+// ---- Hoisting --- //
 
-getOriginal();
+//Example of hoisting:
+
+//--> The output here for x will be undefined because the varibale is present in the global environment and x variable is initialized only after x is been used up so according to hoisting we must declare the functions and varibles at the top of the respective scopes.
+
+/*--->>
+
 console.log(x);
 console.log(getName);
 var x = 7;
+
+<<---*/
+
+// Functions examples
+
+/*--->>
+
 // -----> variable function decleration
+
 var getName2 = function () {
     console.log("hi");
 }
+getName2();
+
 // ----> arrow function
 var getName = () => {
     console.log("welcome");
 }
+getName();
+
 function getOriginal() {
     console.log("Hello");
 }
+getOriginal();
 
+<<---*/
 
-// -- call stack Example--
-// var x=4;
-// function square(n){
-//     var ans = n*n;
-//     return ans;
-// }
-// var square2 = square(x);
-// var square3 =square(2);
-// console.log(square2);
-// console.log(square3);
+// -- call stack Example program --
+
+/*---
+
+var x = 4;
+function square(n) {
+    var ans = n * n;
+    return ans;
+}
+var square2 = square(x);
+var square3 = square(2);
+console.log(square2);
+console.log(square3);
+
+---*/
